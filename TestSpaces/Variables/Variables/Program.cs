66debd50ddf3557674e5e9eea23ef66904f23b9a
Alphalilly <zoozoosamak@gmail.com>
2021-09-20 12:40:24 -0300

    Poptarts have an amount now

---
+++ TestSpaces/Variables/Variables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variables");
+            Console.ReadKey(true);
+
+            // tis a Data type int. (takes numbers)
+            // said int has an identifier (poptarts) and is assigned (=) a value
+            int poptarts;
+
+            poptarts = 0; // assigned poptarts a value of 0
+            Console.WriteLine("The ammount of poptarts are: " + poptarts);
+            Console.ReadKey(true);
+
+            poptarts = 1000; // assigned poptarts a value of 1000
+            Console.WriteLine("The ammount of poptarts are: " + poptarts);
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
