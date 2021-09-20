e59090ba42334eefc82d62063a417e330c75b3b5
Alphalilly <zoozoosamak@gmail.com>
2021-09-19 23:20:18 -0300

    Hello World 2 Electric boogaloo

---
+++ Assignments/LDMI_HelloWorld/LDMI_HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDMI_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Im back boiiisss! Logic and programing 2 electric boogaloo XD");
+            Console.ReadKey(true);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Also pretty colours");
+            Console.ReadKey(true);
+
+            Console.WriteLine("k im done now bai-"); //your not suposed to see this.. thats the joke. IM FUNNY HAHAHA
+
+        }
+    }
+}
