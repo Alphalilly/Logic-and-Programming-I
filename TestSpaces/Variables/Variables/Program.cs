5a45604a3270c486722f5075ae2471928e372fea
Alphalilly <zoozoosamak@gmail.com>
2021-09-20 12:54:52 -0300

    customer very angy, wants a poptart
    Created a string, and played around with it.

--- TestSpaces/Variables/Variables/Program.cs
+++ TestSpaces/Variables/Variables/Program.cs
@@ -13,6 +13,7 @@ namespace Variables
             Console.WriteLine("Variables");
             Console.ReadKey(true);
 
+            // Achivement: "you have declared a variable!"
             // tis a Data type int. (takes numbers)
             // said int has an identifier (poptarts) and is assigned (=) a value
             int poptarts;
@@ -25,7 +26,20 @@ namespace Variables
             Console.WriteLine("The ammount of poptarts are: " + poptarts);
             Console.ReadKey(true);
 
+            // tis another Data type, string. (takes letters)
+            //said string also has an identifeir (angy) and is assigned a value as well
+            //although instead of being just one value, its a bunch of values that form a letter
+            //its a char array. and a char is basically a single letter or symbole that is made up of ascii numbers.
+            string angy;
+            string veryAngy = "I WANTS A POPTART";
 
+            angy = "no i wants a poptart"; //assigned a string variable
+            Console.WriteLine(angy);
+            Console.ReadKey(true);
+
+            angy = "hey gimme poptart "; //assigned a string variable
+            Console.WriteLine(angy + veryAngy);
+            Console.ReadKey(true);
         }
     }
 }
