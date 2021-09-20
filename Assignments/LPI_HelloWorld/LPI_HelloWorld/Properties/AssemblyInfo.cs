753b77f1584fe463abff77c203dec7625311e351
Alphalilly <zoozoosamak@gmail.com>
2021-09-19 23:32:26 -0300

    Hello World 2 Electric Boogaloo (Wrong File name dunce XD)
    dont worry nothing broke right?

--- Assignments/LPI_HelloWorld/LPI_HelloWorld/Properties/AssemblyInfo.cs
+++ Assignments/LPI_HelloWorld/LPI_HelloWorld/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LDMI_HelloWorld")]
+[assembly: AssemblyTitle("LPI_HelloWorld")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LDMI_HelloWorld")]
+[assembly: AssemblyProduct("LPI_HelloWorld")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("218ac9a5-e140-4884-8f53-dcd737674e7d")]
+[assembly: Guid("92771793-555e-4940-bbea-ca30a835c79a")]
 
 // Version information for an assembly consists of the following four values:
 //
