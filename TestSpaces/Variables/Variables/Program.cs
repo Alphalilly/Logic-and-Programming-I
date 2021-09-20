74edea7a59d7cd2524a3274974a3274a7cf008d5
Alphalilly <zoozoosamak@gmail.com>
2021-09-20 13:42:36 -0300

    Poptarts have (hypothetical) prices now :3

--- TestSpaces/Variables/Variables/Program.cs
+++ TestSpaces/Variables/Variables/Program.cs
@@ -14,8 +14,10 @@ namespace Variables
             Console.ReadKey(true);
 
             // Achivement: "you have declared a variable!"
+
             // tis a Data type int. (takes numbers)
             // said int has an identifier (poptarts) and is assigned (=) a value
+            //holds a whole number, cant store decimal numbers
             int poptarts;
 
             poptarts = 0; // assigned poptarts a value of 0
@@ -39,6 +41,21 @@ namespace Variables
 
             angy = "hey gimme poptart "; //assigned a string variable
             Console.WriteLine(angy + veryAngy);
+            Console.ReadKey(true);
+
+            // tis yet another Data type. float (takes numbers with decimals)
+            // said float also has an identifeir (price) and is assigned a value of a decimal number. with (f) telling the program that its type float
+            // this is f*king weird cuz i guess its set up in a way to tell the computer that its a decimal number.
+            // so i guess a float can technically be bigger then an int, dispite holding decimal numbers
+            // yes scary math things https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
+            float price;
+
+            price = 1.23f; // assigned price a decimal value
+            Console.WriteLine("Chocolate poptart is: $" + price);
+            Console.ReadKey(true);
+
+            price = 1.23f + 3; // assigned price a decimal value, adding a whole number to it
+            Console.WriteLine("Chocolate poptart and orenge poptart is: $" + price);
             Console.ReadKey(true);
         }
     }
