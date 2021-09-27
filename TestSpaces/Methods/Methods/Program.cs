93a626ef22111b43047fb0f72efa4afeb9d73d2c
Alphalilly <zoozoosamak@gmail.com>
2021-09-27 12:42:08 -0300

    funny poptart hud

--- TestSpaces/Methods/Methods/Program.cs
+++ TestSpaces/Methods/Methods/Program.cs
@@ -8,38 +8,51 @@ namespace Methods
 {
     class Program
     {
-
+        //declarations
+        static int money;
+        static string name;
+        static int poptarts;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Methods"); //AKA procedures, functions, routines etc.
             Console.ReadKey(true);
 
-            int money;
-            string name;
-            int poptarts;
-
             //init
             name = "hhhhhheeeeeleeeeeen";
             money = 0;
             poptarts = 0;
 
-            //hud
-            Console.WriteLine("name = " + name);
-            Console.WriteLine("score = " + money);
-            Console.WriteLine("xp = " + poptarts);
-            Console.ReadKey(true);
+            ShowHUD(); //very clean lol
+
+            ////hud
+            //Console.WriteLine("name = " + name);
+            //Console.WriteLine("score = " + money);
+            //Console.WriteLine("xp = " + poptarts);
+            //Console.WriteLine();
+            //Console.ReadKey(true);
 
             //pretend gameplay
             money = money + 200;
             poptarts = poptarts + 5;
 
-            //hud
+            ShowHUD();
+
+            ////hud
+            //Console.WriteLine("name = " + name);
+            //Console.WriteLine("score = " + money);
+            //Console.WriteLine("xp = " + poptarts);
+            //Console.ReadKey(true);
+            //// ^ this dumb. copy paste copy paste copy paste copy paste. no
+        }
+        // a method that showes a HUD
+        static void ShowHUD()
+        {
             Console.WriteLine("name = " + name);
             Console.WriteLine("score = " + money);
             Console.WriteLine("xp = " + poptarts);
+            Console.WriteLine();
             Console.ReadKey(true);
-            // ^ this dumb. copy paste copy paste copy paste copy paste. no
         }
     }
 }
