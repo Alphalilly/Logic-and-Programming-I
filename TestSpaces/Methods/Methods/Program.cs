0bb8a7d4fcce546b0f153cef382ad4d1242eb689
Alphalilly <zoozoosamak@gmail.com>
2021-09-27 12:25:49 -0300

    more poptarts

--- TestSpaces/Methods/Methods/Program.cs
+++ TestSpaces/Methods/Methods/Program.cs
@@ -8,8 +8,38 @@ namespace Methods
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Methods"); //AKA procedures, functions, routines etc.
+            Console.ReadKey(true);
+
+            int money;
+            string name;
+            int poptarts;
+
+            //init
+            name = "hhhhhheeeeeleeeeeen";
+            money = 0;
+            poptarts = 0;
+
+            //hud
+            Console.WriteLine("name = " + name);
+            Console.WriteLine("score = " + money);
+            Console.WriteLine("xp = " + poptarts);
+            Console.ReadKey(true);
+
+            //pretend gameplay
+            money = money + 200;
+            poptarts = poptarts + 5;
+
+            //hud
+            Console.WriteLine("name = " + name);
+            Console.WriteLine("score = " + money);
+            Console.WriteLine("xp = " + poptarts);
+            Console.ReadKey(true);
+            // ^ this dumb. copy paste copy paste copy paste copy paste. no
         }
     }
 }
