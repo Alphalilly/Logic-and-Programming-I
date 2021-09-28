522c3309a043df58a92302489a8de100f88ffb30
Alphalilly <zoozoosamak@gmail.com>
2021-09-28 10:48:06 -0300

    Method HUD assignment completed
    also forgot the .gitnore on my last commit. I hope that dosnt break anything ><

--- Assignments/LPI_Methods_HUDv1.1/LPI_Methods_HUDv1.1/Program.cs
+++ Assignments/LPI_Methods_HUDv1.1/LPI_Methods_HUDv1.1/Program.cs
@@ -12,189 +12,194 @@ namespace LPI_Methods_HUDv1._1
 		// Variables: health, score, attack, lives. damage.
 		// Methods:  ShowHUD(), TakeDamage(int damage), AddScore(int enemyValue, int scoreMultiplier)
 		// operators: Score Multiplier. Starting with (x2) and muliplies in by the power of 2 (x4, x6, x8, x10)
-		// extra: Formatted Ascii text, Coloured text, a score subtractor/divider? (when damage taken, lose score beased on damage taken)
+		// extra: Formatted Ascii text, more methods, Coloured text, a score subtractor/divider? (when damage taken, lose score beased on damage taken)
 
-		//Variables
+		//Variables (is a thing)
 		static int health;
-		static int attack; //player attack
+		static int attack;
+		static float score;
+
 		static int lives;
+
+		static float points;
+		static int damage;
 		static int heal;
-		static float score; // overall score
+		static int powerUp;
 
-		static float points; //incoming points (to be multiplied)
+		static void Main(string[] args) // do I even need args?
+		{
+			Game game = new Game();
 
-		static void ShowHUD()
-        {
-			Console.WriteLine("Player name: Dumb Protagonist");
-			Console.WriteLine("Health: " + health);
-			Console.WriteLine("Attack Power: " + attack);
-			Console.WriteLine("Lives: " + lives);
-			Console.WriteLine("Score: " + score);
+			game.GameStart();
 		}
 
-		static void takeDamage(int damage) //enemy attack
-		{
-			health = health - damage;
-        }
-
-		static void AddScore(int points, int scoreMulti)
+		class Game
         {
-			score = score + points * scoreMulti; //sortaaaa
-        }
-
-
-		static void Main(string[] args)
-		{
-			game();
-
-		}
-
-		
-
-
-		static void game()
-		{
-			//starting values 
-			health = 100;
-			attack = 50;
-			heal = 10;
-			lives = 3;
-			score = 200.7f;
-
-			//output
-			Console.WriteLine("> NSCC Game Dev");
-			Console.WriteLine("> Totally not a Game");
-			Console.ReadKey(true);
-
-			Console.WriteLine();
-
-			ShowHUD();
-			Console.ReadKey(true);
-
-			Console.WriteLine();
-
-			void event1()
+			//methods (does a thing)
+			static void ShowHUD()
 			{
-				Console.WriteLine("An Enemey Has appeared!");
-				Console.ReadKey(true);
-
-				Console.WriteLine();
-
-				Console.WriteLine("Player Attacked! Player dealt " + attack + " in damage to Enemy");
-				Console.ReadKey(true);
-
-				Console.WriteLine();
-
-				points = 5.5f;
-				Multiplier(1); //no multiplier curently applied
-				Console.WriteLine("Player gained " + points + " Score");
-				Console.ReadKey(true);
-
-				Console.WriteLine();
-
-				ShowHUD();
-				Console.ReadKey(true);
-
+				Console.WriteLine("Player name: Dumb Protagonist");
+				Console.WriteLine("Health: " + health);
+				Console.WriteLine("Attack Power: " + attack);
+				Console.WriteLine("Lives: " + lives);
+				Console.WriteLine("Score: " + score);
 				Console.WriteLine();
 			}
 
-			void event2()
+			static void takeDamage(int damage)
 			{
 				health = health - damage;
-				Console.WriteLine("Enemy Attacked! Player has taken " + damage + " in damage From Enemy");
-				Console.ReadKey(true);
+			}
 
-				Console.WriteLine();
-
-				ShowHUD();
-				Console.ReadKey(true);
-
-				Console.WriteLine();
-
+			static void Heal(int heal)
+			{
 				health = health + heal;
-				Console.WriteLine("Player healed " + heal + " HP");
-				Console.ReadKey(true);
-
-				Console.WriteLine();
-
-				ShowHUD();
-				Console.ReadKey(true);
-
-				Console.WriteLine();
 			}
 
-			void event3()
+			static void PowerUp(int powerUp)
+            {
+				attack = attack + powerUp;
+			}
+
+			static void Revive()
+            {
+				lives = lives - 1; //future refrence. if lives = 0. then dont run method. or run game over method.
+				health = 100;
+			}
+
+			static void AddScore(float mPoints, int scoreMulti) 
 			{
-				damage = damage + 70; //make damage 70 here
-				health = health - damage;
-				Console.WriteLine("Enemy Attacked! Player has take " + damage + " in damage From Enemy, its a critical hit!");
-				Console.ReadKey(true);
-
-				Console.WriteLine();
-
-				ShowHUD();
-				Console.ReadKey(true);
-
-				Console.WriteLine();
-
-				lives = lives - 1;
-				health = health + 100;
-				Console.WriteLine("Player Revives! " + lives + " Lives Remain");
-				Console.ReadKey(true);
-
-				Console.WriteLine();
-
-				ShowHUD();
-				Console.ReadKey(true);
-
-				Console.WriteLine();
+				mPoints = mPoints * scoreMulti;
+				points = mPoints; 
+				// ^^^ I did it this way instead of just {score + mpoints * scoreMulti} so what I did on line 187 would work, and display the doubled points properly.
+				score = score + points;
 			}
 
-			void event4()
+			public void GameStart()
 			{
-				attack = attack + 100;
-				Console.WriteLine("Player Attacked! Player dealt " + attack + " in damage to Enemy. Its a Critical hit!");
-				Console.ReadKey(true);
+				//starting values 
+				health = 100;
+				attack = 50;
+				lives = 3;
+				score = 200.7f;
 
+				//output
+				Console.WriteLine("> NSCC Game Dev");
+				Console.WriteLine("> Totally not a Game");
 				Console.WriteLine();
-
-				Console.WriteLine("Enemy Dies");
 				Console.ReadKey(true);
-
-				Console.WriteLine();
-
-
-				points = 50.5f;
-				Console.WriteLine("Player gained " + points + " points");
-				Console.ReadKey(true);
-
-				Console.WriteLine();
-
-				Multiplier(2); //score multiplier of 2 currently applied
-				Console.WriteLine("Player points doubled! now is " + points);
-				Console.ReadKey(true);
-
-
-				Console.WriteLine();
 
 				ShowHUD();
 				Console.ReadKey(true);
 
-				Console.WriteLine();
+				void event1()
+				{
+					Console.WriteLine("An Enemey Has appeared!");
+					Console.WriteLine();
+					Console.ReadKey(true);
 
+					Console.WriteLine("Player Attacked! Player dealt " + attack + " in damage to Enemy");
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					points = 5.5f;
+					AddScore(points, 1);
+					Console.WriteLine("Player gained " + points + " Score");
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					ShowHUD();
+					Console.ReadKey(true);
+				}
+
+				event1();
+
+				void event2()
+				{
+					damage = 20; // me small brain
+					takeDamage(damage); // DAMN IT YOU CAN FAAKING PUT VARIABLES IN THE BRACKETS OF METHODS W H A T?!
+					Console.WriteLine("Enemy Attacked! Player has taken " + damage + " in damage From Enemy");
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					ShowHUD();
+					Console.ReadKey(true);
+
+					heal = 10;
+					Heal(heal);
+					Console.WriteLine("Player healed " + heal + " HP");
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					ShowHUD();
+					Console.ReadKey(true);
+				}
+
+				event2();
+
+				void event3()
+				{
+					damage = 90; //note to self. do range cheking for values and prevent overflow into the negative values.
+					takeDamage(damage);
+					Console.WriteLine("Enemy Attacked! Player has take " + damage + " in damage From Enemy, its a critical hit!");
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					ShowHUD();
+					Console.ReadKey(true);
+
+					Revive();
+					Console.WriteLine("Player Revives! " + lives + " Lives Remain");
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					ShowHUD();
+					Console.ReadKey(true);
+				}
+
+				event3();
+
+				void event4()
+				{
+					powerUp = 100;
+					PowerUp (powerUp);
+					Console.WriteLine("Player gains a power up! Attack Power increased by " + powerUp);
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					ShowHUD();
+					Console.ReadKey(true);
+
+					Console.WriteLine("Player Attacked! Player dealt " + attack + " in damage to Enemy. Its a Critical hit!");
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					Console.WriteLine("Enemy Dies");
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+
+					points = 50.5f;
+					Console.WriteLine("Player gained " + points + " points");
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					AddScore(points, 2);
+					Console.WriteLine("Player points doubled! now is " + points);
+					Console.WriteLine();
+					Console.ReadKey(true);
+
+					ShowHUD();
+					Console.ReadKey(true);
+				}
+
+				event4();
+
+				Console.WriteLine("> Game End");
+				Console.ReadKey(true);
 			}
-
-			event1();
-
-			event2();
-
-			event3();
-
-			event4();
-
-			Console.WriteLine("> game End");
-			Console.ReadKey(true);
 		}
+		
 	}
 }
 
