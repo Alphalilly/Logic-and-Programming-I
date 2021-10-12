6ce5ff2ff5c85d78cd845e962db06269d92a9c1c
Alphalilly <zoozoosamak@gmail.com>
2021-10-12 16:57:05 -0300

    Ammo System assignment
    aaaaaaa I didnt do it riiiiiggght T^T

--- Assignments/LPI_Arrrays_AmmoSystem/LPI_Arrrays_AmmoSystem/Program.cs
+++ Assignments/LPI_Arrrays_AmmoSystem/LPI_Arrrays_AmmoSystem/Program.cs
@@ -8,9 +8,118 @@ namespace LPI_Arrrays_AmmoSystem
 {
     class Program
     {
+        public static int[] weapon = new int[5]; //this means that there are 5 slots. not the slots in the index go up to the number 5
+        //I keep getting a NullReferenceException when I do this. and I dont know why >< 
+        // https://stackoverflow.com/questions/779091/what-does-object-reference-not-set-to-an-instance-of-an-object-mean
+        //{
+        //    weapon[0] = 1,
+        //    weapon[1] = 2,
+        //    weapon[2] = 3,
+        //    weapon[3] = 4,
+        //    weapon[4] = 5
+        //};
+
+        static int[] ammo = new int[5];
+        //here too
+        //{
+        //    ammo[0] = 20,
+        //    ammo[1] = 10,
+        //    ammo[2] = 30,
+        //    ammo[3] = 50,
+        //    ammo[4] = 5
+        //};
+
+        static string[] weaponName = new string[5];
+        // and here. so I just moved it back in Main()
+        //{
+        //    weaponName[0] = "Pistol",
+        //    weaponName[1] = "Shotgun",
+        //    weaponName[2] = "Sniper",
+        //    weaponName[3] = "Lazer Gun",
+        //    weaponName[4] = "BFG"
+        //};
+
+
+        static void Main(string[] args)
+        {
+            //weapon
+            weapon[0] = 1;
+            weapon[1] = 2;
+            weapon[2] = 3;
+            weapon[3] = 4;
+            weapon[4] = 5;
+
+            //weapon ammo
+            ammo[0] = 20;
+            ammo[1] = 10;
+            ammo[2] = 30;
+            ammo[3] = 50;
+            ammo[4] = 5;
+            
+            //weapon name
+            weaponName[0] = "Pistol";
+            weaponName[1] = "Shotgun";
+            weaponName[2] = "Sniper";
+            weaponName[3] = "Lazer Gun";
+            weaponName[4] = "BFG";
+
+            Fire(weapon[0]); //hol' up. Why does the output say "shot gun" and ammo "10", when I clearly specified weapon to be 0 in the array?
+                             // aaaa i dont have time to think of an answer.
+            Console.ReadKey(true);
+
+            Reload(weapon[0]);
+
+            Console.ReadKey(true);
+
+        }
+
+        // would be nice to create method that displays all the weapon status
+
+        static void Fire(int weapon) //decrements ammo
+        {
+            Console.WriteLine("Fired " + weaponName[weapon] + " With ammo " + ammo[weapon]);
+
+            //if ammo is more then 0
+            if (ammo[weapon] < 0)
+            {
+                ammo[weapon]--;
+            }
+            else
+            {
+
+            }
+
+            Console.WriteLine("now " + weaponName[weapon] + " With ammo " + ammo[weapon]); //fpr some reason the ammo value dosnt change dispite being decremented ><
+
+        }
+
+        static void Reload(int weapon) //adds ammo
+        {
+            Console.WriteLine("Relaod " + weaponName[weapon] + " With ammo " + ammo[weapon]);
+
+            //if ammo is less then itself
+
+            if (ammo[weapon] > ammo[weapon])
+            {
+                ammo[weapon]++;
+            }
+            else
+            {
+
+            }
+
+            Console.WriteLine("now " + weaponName[weapon] + " With ammo " + ammo[weapon]);
+        }
+
+    }
+
+    /* my dumb ass's fist attempt omg im dumb
+    class Program
+    {
         static int[] weapon = new int[5]; //this means that there are 5 slots. not the slots in the index go up to the number 5
         static int[] ammo = new int[5];
         static string[] weaponName = new string[5];
+        
 
         static void Main(string[] args)
         {
@@ -58,6 +167,8 @@ namespace LPI_Arrrays_AmmoSystem
             Console.WriteLine("Fired " + weaponName[choiceWeapon] + " With ammo " + ammo[choiceAmmo]); //honestly I could have just put ammo[choiceWeapon] and it would still be the same
             //wait this dosnt work cuz im doing a fucky and puttingthe value of 20 back into the ammo[]
 
+            //if ammo is more then 0
+
             choiceAmmo = choiceAmmo - 1;
 
             Console.WriteLine("now " + weaponName[choiceWeapon] + " With ammo " + ammo[choiceAmmo]);
@@ -68,10 +179,13 @@ namespace LPI_Arrrays_AmmoSystem
         {
             Console.WriteLine("Relaod " + weaponName[choiceWeapon] + " With ammo " + ammo[choiceAmmo]);
 
+            //if ammo is less then itself
+
             choiceAmmo = choiceAmmo + 1;
 
             Console.WriteLine("now " + weaponName[choiceWeapon] + " With ammo " + ammo[choiceAmmo]);
         }
 
     }
+    */
 }
