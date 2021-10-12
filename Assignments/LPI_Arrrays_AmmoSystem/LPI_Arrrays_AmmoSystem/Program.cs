aa16d6794df8b4ce1e2b7c0923073989c2d936a8
Alphalilly <zoozoosamak@gmail.com>
2021-10-12 11:07:12 -0300

    aaaaaaaaaaaa

---
+++ Assignments/LPI_Arrrays_AmmoSystem/LPI_Arrrays_AmmoSystem/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPI_Arrrays_AmmoSystem
+{
+    class Program
+    {
+        static int[] weapon = new int[5]; //this means that there are 5 slots. not the slots in the index go up to the number 5
+        static int[] ammo = new int[5];
+        static string[] weaponName = new string[5];
+
+        static void Main(string[] args)
+        {
+            //Weapons
+            weapon[0] = 1;
+            weapon[1] = 2;
+            weapon[2] = 3;
+            weapon[3] = 4;
+            weapon[4] = 5;
+
+            //Ammo
+            ammo[0] = 20;
+            ammo[1] = 10;
+            ammo[2] = 30;
+            ammo[3] = 50;
+            ammo[4] = 5;
+
+            //weapon Names
+            weaponName[0] = "Pistol";
+            weaponName[1] = "Shotgun";
+            weaponName[2] = "Sniper";
+            weaponName[3] = "Lazer Gun";
+            weaponName[4] = "BFG";
+
+            test(ammo[0]);
+
+            //Fire(weapon[0], ammo[0]); //this is yucky cuz im basically just saying (0, 0). its the same value! but i guessed being used in differnt ways lel
+
+            Console.ReadKey(true);
+
+            //Reload(weapon[0], ammo[0]);
+
+            Console.ReadKey(true);
+        }
+
+        // create method that displays all the weapon status
+
+        static void test(int choiceAmmo)
+        {
+            Console.WriteLine(choiceAmmo);
+        }
+
+        static void Fire(int choiceWeapon, int choiceAmmo) //decriments ammo
+        {
+            Console.WriteLine("Fired " + weaponName[choiceWeapon] + " With ammo " + ammo[choiceAmmo]); //honestly I could have just put ammo[choiceWeapon] and it would still be the same
+            //wait this dosnt work cuz im doing a fucky and puttingthe value of 20 back into the ammo[]
+
+            choiceAmmo = choiceAmmo - 1;
+
+            Console.WriteLine("now " + weaponName[choiceWeapon] + " With ammo " + ammo[choiceAmmo]);
+
+        }
+
+        static void Reload(int choiceWeapon, int choiceAmmo) //adds ammo
+        {
+            Console.WriteLine("Relaod " + weaponName[choiceWeapon] + " With ammo " + ammo[choiceAmmo]);
+
+            choiceAmmo = choiceAmmo + 1;
+
+            Console.WriteLine("now " + weaponName[choiceWeapon] + " With ammo " + ammo[choiceAmmo]);
+        }
+
+    }
+}
---
+++ TestSpaces/Arrays/Arrays/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before Arrays");
+            // STORE AMMO OF ALL WEAPONS
+            //pistol = 6/6
+            //shotgun = 2/2
+            // etc
+            int weapon = 1; //WEAPON INDEX. 0 = pistol, 1 = shotgun, etc
+
+            int pistolAmmo = 6;
+            int shotgunAmmo = 2;
+
+            //how to show the ammo of current weapon (without hardcoding)
+
+            if (weapon == 0)
+            {
+                Console.WriteLine("pistol ammo " + pistolAmmo);
+            }
+            else if (weapon == 1)
+            {
+                Console.WriteLine("shot Gun " + shotgunAmmo);
+            }
+            //etc
+
+            Console.WriteLine("Arrays");
+            //its litrally an Array of something. a way to store a list of something (except lists are also its own thing)
+            // better and effeicant way to store variables
+
+            int[] ammo; //declaration (ammo is an array of ints)
+            ammo = new int[3]; //initialisation 1 (initialisation of the index. hpw much room it has)
+            ammo[0] = 6; // pistol ammo // initialisation 2 (initialisation of the values in the array) 
+            ammo[1] = 2; //shotgun ammo
+
+            string[] weaponName = new string[2];
+            weaponName[0] = "pistol";
+            weaponName[1] = "shotgun";
+
+            //how to show the ammo of current weapon (without hardcoding)
+
+            Console.WriteLine("pistol ammo " + ammo[0]); //hard coded
+            Console.WriteLine("shotgun ammo " + ammo[1]); //hard coded
+
+            if (weapon == 0)
+            {
+                Console.WriteLine("pistol ammo " + ammo[0]); 
+
+            }
+            else if (weapon == 1)
+            {
+                Console.WriteLine("shotgun ammo " + ammo[1]);
+            }
+
+            //very cool solution this is better XD
+            Console.WriteLine(weaponName[weapon] + ammo[weapon]); //displays the ammo for the current weapon (without all the "if" noise)
+            // well heck i guess you can put other ints into arrays XDDD
+            // not to misunderstand. int weapon is only storeing the number 1 (currently). 
+            // so pluggging in int weapon into ammo[] or weaponName[] is the same as putting in ammo[1] or weaponName[1]
+            // it just looks more elegent in practice to use the already int weapon with the value of 1, then just ammo[1]
+
+            
+            args[0] = "this";
+            args[1] = "is";
+            args[3] = "an";
+            args[4] = "array";
+
+            void weapon(int[] ammo) // so i guess i can do this too
+            {
+
+            }
+        }
+    }
+}
