cf23f23641b069d5c220f88de072709ce2a66c29
Alphalilly <zoozoosamak@gmail.com>
2021-11-09 10:57:39 -0400

    𝓵𝓲𝓼𝓽𝓼

---
+++ Assignments/LPI_Lists_Inventory/LPI_Lists_Inventory/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPI_Lists_Inventory
+{
+    /* Challenge - C# Fundamentals: Lists (Inventory System v1.0):*
+       - Code and showcase the usages of an inventory system using C# Lists
+
+       * Technical Specifications: *
+
+       - Use a List of strings
+       - Have an item maximum
+
+       - PickUp() -- respects maximum limitations.
+       - Drop()
+       - Use() -- assume using an item destroys it if you wish.
+       - ClearInventory() -- used on player death.
+       - ShowInventory() -- could be called at bottom of all other methods.
+
+       - Simulated Gameplay to showcase code functionality
+       - Range checking
+       - Error Checking
+       - Demonstrate storing multiples of the same item.
+       - Do not access list functionality in main. (use methods)
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Test = new List<string>(8);
+            //sets the capacity to 8 in the parameters.
+            // If I dont set the Capacity then it will automatially keep expanding i guess. the more stuff I put in the list.
+            // it expands nontheless. and it multiplies to capacity by 8. i guess it dosnt work the same as arrays?
+
+
+            foreach (string item in Test)
+            {
+                Console.WriteLine(item);
+            }
+
+            if (Test.Count != 8)
+            {
+                Test.Add("a");
+                Test.Add("b");
+                Test.Add("c");
+                Test.Add("d");
+                Test.Add("e");
+                Test.Add("f");
+                Test.Add("g");
+                Test.Add("h");
+                Test.Add("i");
+            }
+            else
+            {
+                Console.WriteLine("do nothing");
+                return;
+            }
+
+            foreach (string item in Test)
+            {
+                Console.WriteLine(item);
+            }
+
+            //a loop that adds stuff. and a loop that "lists" stuff?
+
+
+
+
+            Console.WriteLine(Test.Capacity);
+            Console.WriteLine(Test.Count);
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
+//if iList.Capacity = 8. then stop
+// dont lists also have tags or IDs with every item? (item, id)
---
+++ TestSpaces/Lists2/Lists2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string itemName;
+            //string[] Inv = new string[10];
+
+            //itemName = "hp potion";
+
+            ////have something that sets everything in Inv[] to have "-"
+
+            //for (int i = 0; i <= Inv.Length - 1; i++)
+            //{
+            //    Inv[1] = "";
+            //}
+
+            //void a(string pick)
+            //{
+            //    pick = itemName;
+            //    itemName = Inv[0];
+
+            //    if (pick == "-")
+            //    {
+            //        //dont do anything
+            //    }
+            //    else
+            //    {
+            //        // replace - with itemName
+            //    }
+            //}
+
+            Console.WriteLine("Lists");
+
+            List<string> //decloration
+                inv = new List<string>(); //instantiation
+
+            //_ = new List<string>();
+
+            Console.WriteLine("Capacity: " + inv.Capacity);
+            Console.WriteLine("Count: " + inv.Count);
+            inv.Add("health potion");
+            Console.WriteLine("Capacity: " + inv.Capacity);
+            Console.WriteLine("Count: " + inv.Count);
+            inv.Add("ring");
+            Console.WriteLine("Capacity: " + inv.Capacity);
+            Console.WriteLine("Count: " + inv.Count);
+            inv.Add("bottle");
+            Console.WriteLine("Capacity: " + inv.Capacity);
+            Console.WriteLine("Count: " + inv.Count);
+            inv.Add("pen");
+            Console.WriteLine("Capacity: " + inv.Capacity);
+            Console.WriteLine("Count: " + inv.Count);
+            inv.Add("door");
+            Console.WriteLine("Capacity: " + inv.Capacity);
+            Console.WriteLine("Count: " + inv.Count);
+
+            Console.WriteLine();
+
+            for (int i = 0; i <= inv.Count - 1; i++) //not as nice
+                Console.WriteLine(inv[i]);
+            Console.WriteLine();
+
+            foreach (string item in inv) //nice
+                Console.WriteLine(item);
+            Console.WriteLine();
+
+            inv.Remove("ring");
+
+            foreach (string item in inv)
+                Console.WriteLine(item);
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
