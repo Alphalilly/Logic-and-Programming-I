87a25c8fcbf64910081407a94d03ba067c1c4ebb
Alphalilly <zoozoosamak@gmail.com>
2021-12-07 00:33:08 -0400

    something

---
+++ TestSpaces/readWritetofile/readWritetofile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace readWritetofile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Story = File.ReadAllLines(@"StoryData.txt");
+
+            for (int i = 0; i < Story.Length; i++)
+            {
+                Console.WriteLine(Story[i]);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
